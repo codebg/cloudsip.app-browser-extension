979c7636a70bdb9c698cd5468ff56ce6a996cc1f
codebg <34414594+codebg@users.noreply.github.com>
2026-09-03 12:48:38 +0200

    Improve WebRTC stability: assets/js/default-config.js

--- assets/js/default-config.js
+++ assets/js/default-config.js
@@ -6,7 +6,11 @@ export const defaultConfig = {
     websocketUrl: 'wss://sip.domain.com:8089/ws',
     sipUri: 'sip:DEMO@sip.domain.com',
     displayName: 'DEMO',
-    password: ''
+    password: '',
+    iceServers: [],
+    sessionTimers: true,
+    reconnectMinSeconds: 2,
+    reconnectMaxSeconds: 30
   },
   settings: {
     autoAnswer: false,
